4145402f39a606227496f507216835a2fc9bb683
J. Santos <jamillo@gmail.com>
2020-10-14 02:04:27 -0300

    Implement install

---
+++ external/protodep/up.go
@@ -0,0 +1,42 @@
+package protodep
+
+import (
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/pkg/errors"
+)
+
+func Up() error {
+	cmd := exec.Command("protodep", "up")
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	io.Copy(os.Stdout, stdout)
+	go io.Copy(os.Stderr, stderr)
+
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+
+	ec := cmd.ProcessState.ExitCode()
+	if ec != 0 {
+		return errors.Errorf("process exited with code: %d", ec)
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.14
 
 require (
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/pkg/errors v0.8.0
+	github.com/pkg/errors v0.8.1
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.4.0
+	github.com/stormcat24/protodep v0.0.0-20200505140716-b02c9ba62816 // indirect
 )
